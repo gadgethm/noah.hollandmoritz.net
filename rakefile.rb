913ae31c05ff547e580bd3b80bc41a7476817ed4
gadgethm <nhollandmoritz@gmail.com>
2018-02-06 09:08:04 -0700

    Rakefile again

--- rakefile.rb
+++ rakefile.rb
@@ -4,8 +4,8 @@ desc "Build Jekyll site and copy files"
 task :build do
   system "bundle exec jekyll build"
   system "mkdir ./../temp"
-  system "cp -r ./_site/* ./../temp && git rm -r -f _site/ && git add --all"
-  system "git checkout master && git rm -r -f ./* && cd ./../ && cp -r ./temp/* ./#{GH_PAGES_DIR}/ && rm -r ./temp/ && cd ./#{GH_PAGES_DIR} && git add --all"
+  system "cp -r ./_site/* ./../temp && git rm -r ./_site/ -f && git add --all"
+  system "git checkout master && git rm -r ./* -f && cd ./../ && cp -r ./temp/* ./#{GH_PAGES_DIR}/ && rm -r ./temp/ && cd ./#{GH_PAGES_DIR} && git add --all"
 end
 
 desc "Build Jekyll site, then launch local test page in firefox"
